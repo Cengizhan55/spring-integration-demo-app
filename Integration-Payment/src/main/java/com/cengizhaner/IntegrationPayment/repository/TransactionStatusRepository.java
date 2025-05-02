c4b0ae69c97de58267a1642cc5917fb3edf0dfd9
Cengizhan55 <cengiz_er_@hotmail.com>
2025-05-02 19:17:59 +0300

    "refactor"

--- Integration-Payment/src/main/java/com/cengizhaner/IntegrationPayment/repository/TransactionStatusRepository.java
+++ Integration-Payment/src/main/java/com/cengizhaner/IntegrationPayment/repository/TransactionStatusRepository.java
@@ -4,8 +4,7 @@ import com.cengizhaner.IntegrationPayment.entity.TransactionStatusEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
+//@Repository
 public interface TransactionStatusRepository extends JpaRepository<TransactionStatusEntity, Long> {
-
-    TransactionStatusEntity findByCorrelationId(String uuid);
+   // TransactionStatusEntity findByCorrelationId(String uuid);
 }
