2237d8a75b97e8a73a5d855c683e1ac52de0daa5
Cengizhan55 <cengiz_er_@hotmail.com>
2025-05-24 00:05:55 +0300

    "refactor/cleaning"

--- Integration-Payment/src/main/java/com/cengizhaner/IntegrationPayment/flow/ApplicationFlows.java
+++ Integration-Payment/src/main/java/com/cengizhaner/IntegrationPayment/flow/ApplicationFlows.java
@@ -27,9 +27,6 @@ public class ApplicationFlows {
         this.kafkaProducerChannelConfig = kafkaProducerChannelConfig;
 
     }
-    
-    
-    
 
     @Bean
     public IntegrationFlow incomingMessageFlow() {
