ffc75fb5b3f96dfa5fd0771656c01e72444cae5d
Cengizhan55 <cengiz_er_@hotmail.com>
2025-06-02 00:00:09 +0300

    "refactor/cleaning"

--- IntegrationProducerDemo/src/main/java/com/cengizhaner/IntegrationProducerDemo/service/TransactionStatusService.java
+++ IntegrationProducerDemo/src/main/java/com/cengizhaner/IntegrationProducerDemo/service/TransactionStatusService.java
@@ -12,7 +12,6 @@ public class TransactionStatusService {
     private static final Logger log = LoggerFactory.getLogger(TransactionStatusService.class);
     private final TransactionStatusRepository transactionStatusRepository;
 
-
     public TransactionStatusService(TransactionStatusRepository transactionStatusRepository) {
         this.transactionStatusRepository = transactionStatusRepository;
     }
@@ -23,8 +22,8 @@ public class TransactionStatusService {
             transactionStatusEntity.setData(data);
             transactionStatusEntity.setTrxConditionFlag("W");
             String correlationId = transactionStatusRepository.save(transactionStatusEntity).getCorrelationId();
+           // log.info("Status: {}, CorrelationId: {}", "W: waiting status", correlationId);
 
-            log.info("Status: {}, CorrelationId: {}", "W: waiting status", correlationId);
             return correlationId;
         } catch (Exception e) {
             log.error("error while saving db , data : " + data);
